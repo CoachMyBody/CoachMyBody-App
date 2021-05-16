ec0f8b99c6ee9b2f7cc2fc143212a4ea9a5886c7
Daniel <kwj9931@gmail.com>
2021-05-16 23:44:17 +0900

    루틴_북마크탭_수정

--- lib/ui/routine/routine.dart
+++ lib/ui/routine/routine.dart
@@ -1,11 +1,14 @@
 import 'package:coach_my_body/constants/translations_key.dart';
 import 'package:coach_my_body/ui/routine/routine_sub_myroutines/routine_sub_myroutines.dart';
 import 'package:coach_my_body/ui/routine/routine_sub_bookmark/routine_sub_bookmark.dart';
+import 'package:coach_my_body/ui/routine/routine_sub_bookmark/routine_sub_bookmark_list.dart';
+
 import 'package:coach_my_body/widgets/custom_tab_pages_widget.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class RoutineScreen extends StatelessWidget {
+  int test = 1;
   final List<CMBPages> _supPages = [
     CMBPages(
       page: RoutineSubMyroutinesScreen(),
@@ -23,6 +26,14 @@ class RoutineScreen extends StatelessWidget {
       title: tr(ROUTINE_MAIN_TITLE),
       pages: _supPages,
       pageNum: _supPages.length,
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            bookmarkSelect();
+          },
+          child: Text('선택'),
+        ),
+      ],
     );
   }
 }
--- lib/ui/routine/routine_sub_bookmark/routine_sub_bookmark.dart
+++ lib/ui/routine/routine_sub_bookmark/routine_sub_bookmark.dart
@@ -3,25 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:coach_my_body/ui/routine/routine_sub_bookmark/routine_sub_bookmark_list.dart';
 import 'package:coach_my_body/ui/routine/routine_sub_bookmark/routine_sub_bookmark_detail.dart';
 
+
 class RoutineSubBookmarkScreen extends StatelessWidget {
   final PageController _pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          RoutineSubBookmarkList(
-            text: tr('0'),
-            route: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => RoutineSubBookmarkDetail()));
-            },
-          ),
-        ],
-      ),
+    return RoutineSubBookmarkList(
+      text: tr('0'),
+      route: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => RoutineSubBookmarkDetail()));
+      },
     );
   }
 }
